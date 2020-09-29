2f625eada7152acab2fdb647aa21492da5c48da3
Elo-Get <60546171+Elo-Get@users.noreply.github.com>
2020-09-29 21:22:02 +0200

    Update get_user_events.js

--- modules/get_user_events.js
+++ modules/get_user_events.js
@@ -1,33 +1,35 @@
-const Events = (calendar) => {
-
-    result = [];
-
-    calendar.events.list({
+const getEvents = async (calendar) => {
+    const res = await calendar.events.list({
         calendarId: 'primary',
         timeMin: (new Date()).toISOString(),
         maxResults: 10,
         singleEvents: true,
         orderBy: 'startTime',
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const events = res.data.items;
-        if (events.length) {
-            events.map((event, i) => {
-                const start = event.start.dateTime || event.start.date;
-                const end = event.end.dateTime || event.end.date;
-                const idEvent = event.id;
-                const summary = event.summary;
-
-                result.push({start: start, end: end, idEvent: idEvent, summary: summary});
-
-            });
-        }
     });
+    return res.data.items;
+}
+
+const Events = async (calendar) => {
+
+    let events = await getEvents(calendar);
+    let result = [];
+
+
+    if (events.length) {
+        events.forEach((value) => {
+            const start = value.start.dateTime || value.start.date;
+            const end = value.end.dateTime  || value.end.date;
+            const idEvent = value.id;
+            const summary = value.summary;
+
+            result.push({start: start, end: end, idEvent: idEvent, summary: summary});
+
+        });
+    }
 
     return result;
 
 }
-
 module.exports = {
     Events
 }
